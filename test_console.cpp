4eb05ba12f695168df567897ad966c7bd7d848c2
Hunter Horsman <kariudo@gmail.com>
2024-02-08 15:59:48 -0500

    build: update

--- test_console.cpp
+++ test_console.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "oiltank.h"
 
+/**
+ * Test calculation of oil tank gallons based on distance sensor readings.
+ *
+ * @param argc number of command line arguments
+ * @param argv array of command line arguments
+ *
+ * @return integer representing the exit status
+ *
+ * @throws None
+ */
 int main(int argc, char* argv[]) {
   double distanceToOil; // cm
   double distanceSensorEmbedded; // cm
@@ -9,8 +19,8 @@ int main(int argc, char* argv[]) {
     distanceToOil = std::stod(argv[1]);
     distanceSensorEmbedded = std::stod(argv[2]);
   } else {
-    distanceToOil = 28.23;
-    distanceSensorEmbedded = 5.8;
+    distanceToOil = 13.00;
+    distanceSensorEmbedded = 2.0;
   }
   std::cout << "Using distance to oil: " << distanceToOil << "cm" << std::endl;
   std::cout << "Using distance oil sensor offset of: " << distanceToOil << "cm" << std::endl;
@@ -21,7 +31,7 @@ int main(int argc, char* argv[]) {
   double tankLength = 60.5;
   double tankWidth = 44;
   double inches = tankHeight - airInches; // convert from cm
-  double gallons = vol_oval_h(inches, tankLength, tankWidth, tankHeight);
+  double gallons = kariudo::oiltank::vol_oval_h(inches, tankLength, tankWidth, tankHeight);
 
   std::cout << "Inches of air: " << airInches << std::endl;
   std::cout << "Inches of oil: " << inches << std::endl;
